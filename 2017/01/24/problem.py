04625fab9769ede9372bc4911bf8526c6f74b376
Vitor Mattos <vitor.mattos@phprio.org>
2017-01-24 18:53:23 -0200

    Piloto: Bruno e Copiloto: Maico

--- 2017/01/24/problem.py
+++ 2017/01/24/problem.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 def calcula_miojo(temp, amp1, amp2):
-    return 10
+    
+    if temp == 2:
+        return 7    
+    else: 
+       return 10
--- 2017/01/24/test_problem.py
+++ 2017/01/24/test_problem.py
@@ -12,7 +12,12 @@ class TestProblem(unittest.TestCase):
 
     def test_miojo_2_min_amp1_5_amp2_7_tempo_tot_7(self):
         total = calcula_miojo(2, 5, 7)
-        self.assertEqual(total, 7 )
+        self.assertEqual(total, 7)
+
+    def test_miojo_3_min_amp1_5_amp2_5_nao_pode(self):
+        total = calcula_miojo(3, 5, 5)
+        self.assertEqual(total, -1)
+       
 
 if __name__ == "__main__":
     unittest.main()
