465364f74191a272f37fc2f9e94e7ff4598a2d15
Vitor Mattos <vitor.mattos@phprio.org>
2017-04-06 19:40:03 -0300

    Piloto: Renata e Copiloto: Vitor

--- 2017/2017 - Harry Potter - Python/problem.py
+++ 2017/2017 - Harry Potter - Python/problem.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-
 
+descontos = {1:0, 2:5, 3:10, 4:15, 5:20}
+
 def livro(liv):
 	liv*=42
 	return liv
 
 def calculadesconto(num_liv):
+	percentual_descontos = descontos[num_liv]
 	if num_liv == 2:
 		return 5
-	return 0
+	return percentual_descontos
